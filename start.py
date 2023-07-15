24b93124598637effe06992a1d1cd47ebf654b72
abhinavtembulkar <abhinavtembulkar@gmail.com>
2023-07-15 12:49:30 +0530

    This is python code

---
+++ start.py
@@ -0,0 +1,8 @@
+import math
+
+# In this program, we are trying to find ceil of input number
+print("Please input a number")
+x = input()
+
+x = float(x)
+print("Ceil of the number {} is".format(x), math.ceil(x))
